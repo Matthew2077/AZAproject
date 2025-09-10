edec3462a475a10973594203994ff990f12be627
Matteo Pasquetti <pasquetti47@gmail.com>
2025-09-10 17:33:45 +0200

    Add files via upload

---
+++ elements/pre-loader.php
@@ -0,0 +1,8 @@
+<div id="preloader">
+    <div class="loading-wave">
+        <div class="loading-bar"></div>
+        <div class="loading-bar"></div>
+        <div class="loading-bar"></div>
+        <div class="loading-bar"></div>
+    </div>
+</div>
---
+++ elements/sidebar.php
@@ -0,0 +1,295 @@
+<div class="ic-sidenav">
+    <div class="ic-sidenav-scroll">
+        <ul class="metismenu" id="menu">
+            <li><a class="has-arrow" href="javascript:void(0);" aria-expanded="false">
+                    <i class="flaticon-home"></i>
+                    <span class="nav-text">Dashboard</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="index.php">Dashboard Light</a></li>
+                    <li><a href="index-2.php">Dashboard Dark</a></li>
+                    <li><a href="dashboard-2.php">Dashboard 2</a></li>
+                    <li><a href="project-page.php">Projects</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                    <li><a href="kanban.php">Kanban</a></li>
+                    <li><a href="message.php">Message</a></li>
+                </ul>
+            </li>
+            <!--AGGIUNTO DA MATTHEW-->
+            <li><a class="has-arrow" href="javascript:void(0);" aria-expanded="false">
+                    <i class="flaticon-home"></i>
+                    <span class="nav-text">Personalizzato</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="interfacciaAZA.php">AZA</a></li>
+                    <li><a href="page-create-user.php">Crea nuovo utente</a></li>
+                    <li><a href="password-forgot.php">Forgot Password</a></li>
+                    <li><a href="Request_Short_Desc.php">Accorcia Descrizioni</a></li>
+                </ul>
+            </li>
+            <li>
+                <a class="has-arrow " href="javascript:void(0);" aria-expanded="false">
+                    <i class="flaticon-cms"></i>
+                    <span class="nav-text">CMS</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="content.php">Content</a></li>
+                    <li><a href="content-add.php">Add Content</a></li>
+                    <li><a href="menu.php">Menus</a></li>
+                    <li><a href="email-template.php">Email Template</a></li>
+                    <li><a href="add-email.php">Add Email</a></li>
+                    <li><a href="blog.php">Blog</a></li>
+                    <li><a href="add-blog.php">Add Blog</a></li>
+                    <li><a href="blog-category.php">Blog Category</a></li>
+                </ul>
+            </li>
+            <li><a class="has-arrow " href="javascript:void(0);" aria-expanded="false">
+                    <i class="flaticon flaticon-user-1"></i>
+                    <span class="nav-text">Profile</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="overview.php">Overview</a></li>
+                    <li><a href="projects.php">Projects</a></li>
+                    <li><a href="projects-details.php">Projects Details</a></li>
+                    <li><a href="campaigns.php">Campaigns</a></li>
+                    <li><a href="documents.php">Documents</a></li>
+                    <li><a href="followers.php">Followers</a></li>
+                    <li><a href="activity.php">Activity</a></li>
+                </ul>
+            </li>
+            <li><a class="has-arrow " href="javascript:void(0);" aria-expanded="false">
+                    <i class="flaticon flaticon-app"></i>
+                    <span class="nav-text">Account</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="overview1.php">Overview</a></li>
+                    <li><a href="settings.php">Settings</a></li>
+                    <li><a href="security.php">Security</a></li>
+                    <li><a href="activity1.php">Activity</a></li>
+                    <li><a href="billing.php">Billing</a></li>
+                    <li><a href="statements.php">Statements</a></li>
+                    <li><a href="referrals.php">Referrals</a></li>
+                    <li><a href="api-keys.php">Api keys</a></li>
+                    <li><a href="logs.php">Logs</a></li>
+                </ul>
+            </li>
+            <li><a class="has-arrow " href="javascript:void(0);" aria-expanded="false">
+                    <i class="flaticon flaticon-app"></i>
+                    <span class="nav-text">AIKit</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="auto-write.php">Auto Write</a></li>
+                    <li><a href="scheduled.php">Scheduled</a></li>
+                    <li><a href="repurpose.php">Repurpose</a></li>
+                    <li><a href="rss.php">RSS</a></li>
+                    <li><a href="chatbot.php">Chatbot</a></li>
+                    <li><a href="fine-tune-models.php">Fine Tune Models</a></li>
+                    <li><a href="prompt.php">Prompt</a></li>
+                    <li><a href="setting.php">Setting</a></li>
+                    <li><a href="import.php">Import</a></li>
+                </ul>
+            </li>
+            <li><a class="has-arrow " href="javascript:void(0);" aria-expanded="false">
+                    <i class="flaticon-user"></i>
+                    <span class="nav-text">User</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="app-profile.php">Profile</a></li>
+                    <li><a href="edit-profile.php">Edit Profile</a></li>
+                    <li><a href="post-details.php">Post Details</a></li>
+                </ul>
+            </li>
+            <li><a class="has-arrow " href="javascript:void(0);" aria-expanded="false">
+                    <i class="flaticon-blog"></i>
+                    <span class="nav-text">Blog</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="blog-post.php">Blog Post</a></li>
+                    <li><a href="blog-home.php">Blog Home</a></li>
+                </ul>
+            </li>
+            <li><a href="pricing.php" class="ai-icon" aria-expanded="false">
+                    <i class="flaticon-price-tag"></i>
+                    <span class="nav-text">Pricing</span>
+                </a>
+            </li>
+            <li class="menu-title">Apps</li>
+            <li><a class="has-arrow " href="javascript:void(0);" aria-expanded="false">
+                    <i class="flaticon-shopping-bag"></i>
+
+                    <span class="nav-text">Ecommerce</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a class="has-arrow" href="javascript:void(0);" aria-expanded="false">Categories</a>
+                        <ul aria-expanded="false">
+                            <li><a href="category-table.php">Category Table</a></li>
+                            <li><a href="add-categary.php">Add Category</a></li>
+                            <li><a href="edit-categary.php">Edit Category</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a class="has-arrow" href="javascript:void(0);" aria-expanded="false">Products</a>
+                        <ul aria-expanded="false">
+                            <li><a href="product-table.php">Product Table</a></li>
+                            <li><a href="add-product.php">Add product</a></li>
+                            <li><a href="edit-product.php">Edit Product</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="has-arrow" href="javascript:void(0);" aria-expanded="false">Shop</a>
+                        <ul aria-expanded="false">
+                            <li><a href="ecom-product-grid.php">Product Grid</a></li>
+                            <li><a href="ecom-product-list.php">Product List</a></li>
+                            <li><a href="ecom-product-detail.php">Product Details</a></li>
+                            <li><a href="ecom-product-order.php">Order</a></li>
+                            <li><a href="ecom-checkout.php">Checkout</a></li>
+                            <li><a href="ecom-invoice.php">Invoice</a></li>
+                            <li><a href="ecom-customers.php">Customers</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <li><a class="has-arrow " href="javascript:void(0);" aria-expanded="false">
+                    <i class="flaticon-rocket"></i>
+                    <span class="nav-text">Projects</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="project-list.php">Project List</a></li>
+                    <li><a href="project-card.php">Project Card</a></li>
+                    <li><a href="add-project.php">Add Project</a></li>
+                </ul>
+            </li>
+            <li><a href="note.php" class="ai-icon" aria-expanded="false">
+                    <i class="flaticon-notes"></i>
+                    <span class="nav-text">Notes</span>
+                </a>
+            </li>
+            <li><a href="file-manger.php" class="ai-icon" aria-expanded="false">
+                    <i class="flaticon-approved"></i>
+                    <span class="nav-text">File Manager</span>
+                </a>
+            </li>
+            <li><a class="has-arrow " href="javascript:void(0);" aria-expanded="false">
+                    <i class="flaticon-phone-book"></i>
+                    <span class="nav-text">Contacts</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="contact-list.php">Contact List</a></li>
+                    <li><a href="contact-card.php">Contact Card</a></li>
+                </ul>
+            </li>
+            <li><a class="has-arrow " href="javascript:void(0);" aria-expanded="false">
+                    <i class="flaticon-email"></i>
+                    <span class="nav-text">Inbox</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="email-compose.php">Compose</a></li>
+                    <li><a href="email-inbox.php">Inbox</a></li>
+                    <li><a href="email-read.php">Read</a></li>
+                </ul>
+            </li>
+            <li><a href="app-calender.php" class aria-expanded="false">
+                    <i class="flaticon-calendar-2"></i>
+                    <span class="nav-text">Calender</span>
+                </a>
+            </li>
+            <li class="menu-title">Components</li>
+            <li><a class="has-arrow " href="javascript:void(0);" aria-expanded="false">
+                    <i class="flaticon-bar-chart"></i>
+                    <span class="nav-text">Charts</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="apex.php">Apex Chart</a></li>
+                    <li><a href="chart-flot.php">Flot</a></li>
+                    <li><a href="chart-morris.php">Morris</a></li>
+                    <li><a href="chart-chartjs.php">Chartjs</a></li>
+                    <li><a href="chart-chartist.php">Chartist</a></li>
+                    <li><a href="chart-sparkline.php">Sparkline</a></li>
+                    <li><a href="chart-peity.php">Peity</a></li>
+                </ul>
+            </li>
+            <li><a class="has-arrow " href="javascript:void(0);" aria-expanded="false">
+                    <i class="flaticon-web"></i>
+                    <span class="nav-text">Bootstrap</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="ui-accordion.php">Accordion</a></li>
+                    <li><a href="ui-alert.php">Alert</a></li>
+                    <li><a href="ui-badge.php">Badge</a></li>
+                    <li><a href="ui-button.php">Button</a></li>
+                    <li><a href="ui-modal.php">Modal</a></li>
+                    <li><a href="ui-button-group.php">Button Group</a></li>
+                    <li><a href="ui-list-group.php">List Group</a></li>
+                    <li><a href="ui-card.php">Cards</a></li>
+                    <li><a href="ui-carousel.php">Carousel</a></li>
+                    <li><a href="ui-dropdown.php">Dropdown</a></li>
+                    <li><a href="ui-popover.php">Popover</a></li>
+                    <li><a href="ui-progressbar.php">Progressbar</a></li>
+                    <li><a href="ui-tab.php">Tab</a></li>
+                    <li><a href="ui-typography.php">Typography</a></li>
+                    <li><a href="ui-pagination.php">Pagination</a></li>
+                    <li><a href="ui-grid.php">Grid</a></li>
+                </ul>
+            </li>
+            <li><a class="has-arrow " href="javascript:void(0);" aria-expanded="false">
+                    <i class="flaticon-puzzle"></i>
+                    <span class="nav-text">Plugins</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="uc-select2.php">Select 2</a></li>
+                    <li><a href="uc-nestable.php">Nestedable</a></li>
+                    <li><a href="uc-noui-slider.php">Noui Slider</a></li>
+                    <li><a href="uc-sweetalert.php">Sweet Alert</a></li>
+                    <li><a href="uc-toastr.php">Toastr</a></li>
+                    <li><a href="map-jqvmap.php">Jqv Map</a></li>
+                    <li><a href="uc-lightgallery.php">Light Gallery</a></li>
+                </ul>
+            </li>
+            <li><a href="widget-basic.php" class aria-expanded="false">
+                    <i class="flaticon-app"></i>
+                    <span class="nav-text">Widget</span>
+                </a>
+            </li>
+            <li><a class="has-arrow " href="javascript:void(0);" aria-expanded="false">
+                    <i class="flaticon-registration"></i>
+                    <span class="nav-text">Forms</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="form-element.php">Form Elements</a></li>
+                    <li><a href="form-wizard.php">Wizard</a></li>
+                    <li><a href="form-ckeditor.php">CkEditor</a></li>
+                    <li><a href="form-pickers.php">Pickers</a></li>
+                    <li><a href="form-validation.php">Form Validate</a></li>
+                </ul>
+            </li>
+            <li><a class="has-arrow " href="javascript:void(0);" aria-expanded="false">
+                    <i class="flaticon-grid"></i>
+                    <span class="nav-text">Table</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="table-bootstrap-basic.php">Bootstrap</a></li>
+                    <li><a href="table-datatable.php">Datatable</a></li>
+                </ul>
+            </li>
+            <li><a class="has-arrow " href="javascript:void(0);" aria-expanded="false">
+                    <i class="flaticon-file"></i>
+                    <span class="nav-text">Pages</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="page-login.php">Login</a></li>
+                    <li><a href="page-register.php">Register</a></li>
+                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">Error</a>
+                        <ul aria-expanded="false">
+                            <li><a href="page-error-400.php">Error 400</a></li>
+                            <li><a href="page-error-403.php">Error 403</a></li>
+                            <li><a href="page-error-404.php">Error 404</a></li>
+                            <li><a href="page-error-500.php">Error 500</a></li>
+                            <li><a href="page-error-503.php">Error 503</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="page-lock-screen.php">Lock Screen</a></li>
+                    <li><a href="empty-page.php">Empty Page</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
